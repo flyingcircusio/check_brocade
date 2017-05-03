701dc107c3d45d99c14e1ff3852e5a406809ef8b
Christian Theune <ct@flyingcircus.io>
2017-05-03 09:53:18 +0200

    Progress:
    - clean up main script entry point and use argparse
    - make username and password configurable
    - make rbridge to report selectable (so you can always point to the cluster address)
    - give out log timestamps in real timezone

--- src/check_brocade/__init__.py
+++ src/check_brocade/__init__.py
@@ -4,12 +4,7 @@ import datetime
 import pytz
 import requests
 import sys
-import xml.etree
-
-# TODO
-# show in local time (and say so) 
-# show log in local time and better formatting
-# status check
+import argparse
 
 
 LOCAL_TIME = pytz.timezone('Europe/Berlin')
@@ -35,208 +30,248 @@ ns = {'clock': 'urn:brocade.com:mgmt:brocade-clock',
       'raslog': 'urn:brocade.com:mgmt:brocade-ras-ext',
       'vcs': 'urn:brocade.com:mgmt:brocade-vcs'}
 
-host_fqdn = sys.argv[1]
-host = host_fqdn.split('.')[0]
 
+def main():
+    parser = argparse.ArgumentParser(
+        description='Check status of Brocade VDX cluster.')
+    parser.add_argument('address')
+    parser.add_argument('rbridge')
+    parser.add_argument('username')
+    parser.add_argument('password')
+    args = parser.parse_args()
 
-c = requests.Session()
-c.auth = ('user', 'insulate-aye-aquiline')
+    cluster_address = args.address
+    rbridge_id = args.rbridge
 
-print('Brocade VDX status - {}\n'.format(host))
+    c = requests.Session()
+    c.auth = (args.username, args.password)
 
-print('System\n')
+    r = c.post('http://{}/rest/operational-state/show-vcs'.format(cluster_address),
+               data='<show-vcs></show-vcs>').content
+    r = ElementTree.fromstring(r)
+    for rbridge in r.findall('.//vcs:vcs-node-info', ns):
+        this_id = rbridge.find('./vcs:node-rbridge-id', ns).text
+        if this_id == rbridge_id:
+            host = rbridge.find('./vcs:node-switchname', ns).text
+            break
+    else:
+        raise KeyError("Unknown rbridge: {}".format(rbridge_id))
 
-system = PrettyTable()
-system.field_names = ['item', 'info', 'status']
-system.header = False
-system.align = 'l'
+    print('Brocade VDX status - {}\n'.format(host))
 
-r = c.post('http://{}/rest/operational-state/show-clock'.format(host_fqdn), data='<show-clock></show-clock>').content
-r = ElementTree.fromstring(r)
-time = r.find('.//clock:current-time', ns).text
-zone = r.find('.//clock:timezone', ns).text
-tz = pytz.timezone(zone)
-time = time.split('+')[0]
-time = datetime.datetime.strptime(time, '%Y-%m-%dT%H:%M:%S').replace(tzinfo=tz)
-time = time.astimezone(LOCAL_TIME)
+    print('System\n')
 
-now = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
-offset = abs(now - time).total_seconds()
+    system = PrettyTable()
+    system.field_names = ['item', 'info', 'status']
+    system.header = False
+    system.align = 'l'
 
-time_state = OK if offset < 5 else WARN if offset < 30 else CRIT
-check_status.update(time_state)
-time_state = 'OK' if time_state == OK else 'WARN' if time_state == 'WARN' else 'CRIT' if time_state == 'CRIT' else '?'
+    r = c.post('http://{}/rest/operational-state/show-clock'.format(cluster_address),
+               data='<show-clock><rbridge-id>{}</rbridge-id></show-clock>'.format(rbridge_id)).content
 
-system.add_row(['Current time', time.strftime('%Y-%m-%d %H:%M:%S'), time_state])
+    r = ElementTree.fromstring(r)
+    time = r.find('.//clock:current-time', ns).text
+    zone = r.find('.//clock:timezone', ns).text
+    tz = pytz.timezone(zone)
+    time = time.split('+')[0]
+    time = datetime.datetime.strptime(time, '%Y-%m-%dT%H:%M:%S').replace(tzinfo=tz)
+    time = time.astimezone(LOCAL_TIME)
 
-r = c.post('http://{}/rest/operational-state/get-last-config-update-time'.format(host_fqdn), data='<get-last-config-update-time></get-last-config-update-time>').content
-r = ElementTree.fromstring(r)
-timestamp = int(r.find('.//vcs:last-config-update-time', ns).text)
-last_change = datetime.datetime.fromtimestamp(timestamp, tz).astimezone(LOCAL_TIME)
-system.add_row(['Last config change', last_change.strftime('%Y-%m-%d %H:%M:%S'), ''])
+    now = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+    offset = abs(now - time).total_seconds()
 
-r = c.post('http://{}/rest/operational-state/get-system-uptime'.format(host_fqdn), data='<get-system-uptime></get-system-uptime>').content
-r = ElementTree.fromstring(r)
-days = int(r.find('.//system:days', ns).text)
-hours = int(r.find('.//system:hours', ns).text)
-minutes = int(r.find('.//system:minutes', ns).text)
-seconds = int(r.find('.//system:seconds', ns).text)
+    time_state = OK if offset < 5 else WARN if offset < 30 else CRIT
+    check_status.update(time_state)
+    time_state = ('OK' if time_state == OK
+                  else 'WARN' if time_state == WARN
+                  else 'CRIT' if time_state == CRIT
+                  else '?')
 
-total_uptime = days * 24
-total_uptime = (total_uptime + hours) * 60
-total_uptime = (total_uptime + minutes) * 60
-total_uptime = (total_uptime + seconds)
+    system.add_row(['Current time', time.strftime('%Y-%m-%d %H:%M:%S'),
+                   time_state])
 
-uptime_status = 'OK'
-if total_uptime < (30*60):
-    check_status.update(CRIT)
-    uptime_status = 'CRIT'
-elif total_uptime < (2*60*60):
-    check_status.update(WARN)
-    uptime_status = 'WARN'
+    r = c.post('http://{}/rest/operational-state/get-last-config-update-time'.
+               format(cluster_address),
+               data='<get-last-config-update-time></get-last-config-update-time>'
+               ).content
+    r = ElementTree.fromstring(r)
+    timestamp = int(r.find('.//vcs:last-config-update-time', ns).text)
+    last_change = datetime.datetime.fromtimestamp(
+        timestamp, tz).astimezone(LOCAL_TIME)
+    system.add_row(['Last config change',
+                    last_change.strftime('%Y-%m-%d %H:%M:%S'), ''])
 
-uptime = '{} days {:02d}:{:02d}:{:02d}'.format(days, hours, minutes, seconds)
-system.add_row(['Uptime', uptime, uptime_status])
+    r = c.post('http://{}/rest/operational-state/get-system-uptime'.
+               format(cluster_address), data='<get-system-uptime><rbridge-id>{}</rbridge-id></get-system-uptime>'.format(rbridge_id)
+               ).content
+    r = ElementTree.fromstring(r)
+    days = int(r.find('.//system:days', ns).text)
+    hours = int(r.find('.//system:hours', ns).text)
+    minutes = int(r.find('.//system:minutes', ns).text)
+    seconds = int(r.find('.//system:seconds', ns).text)
 
+    total_uptime = days * 24
+    total_uptime = (total_uptime + hours) * 60
+    total_uptime = (total_uptime + minutes) * 60
+    total_uptime = (total_uptime + seconds)
 
+    uptime_status = 'OK'
+    if total_uptime < (30 * 60):
+        check_status.update(CRIT)
+        uptime_status = 'CRIT'
+    elif total_uptime < (2 * 60 * 60):
+        check_status.update(WARN)
+        uptime_status = 'WARN'
 
-r = c.post('http://{}/rest/operational-state/show-system-monitor'.format(host_fqdn), data='<show-system-monitor></show-system-monitor>').content
-r = ElementTree.fromstring(r)
-state = r.find('.//system-monitor:switch-state', ns).text
-state = 'OK' if state == 'state-healthy' else 'CRIT'
-state_info = r.find('.//system-monitor:switch-state-reason', ns).text
-state_info = state_info.rstrip()
+    uptime = '{} days {:02d}:{:02d}:{:02d}'.format(days, hours, minutes, seconds)
+    system.add_row(['Uptime', uptime, uptime_status])
 
-system.add_row(['Switch state', state_info, state])
+    r = c.post('http://{}/rest/operational-state/show-system-monitor'.format(
+        cluster_address), data='<show-system-monitor><rbridge-id>{}</rbridge-id></show-system-monitor>'.format(rbridge_id)).content
+    r = ElementTree.fromstring(r)
+    state = r.find('.//system-monitor:switch-state', ns).text
+    state = 'OK' if state == 'state-healthy' else 'CRIT'
+    state_info = r.find('.//system-monitor:switch-state-reason', ns).text
+    state_info = state_info.rstrip()
 
-for component in r.findall('.//system-monitor:component-status', ns):
-    name = component.find('./system-monitor:component-name', ns).text
-    name = name.replace(' monitor', '')
-    status_raw = component.find('./system-monitor:component-state', ns).text
-    if status_raw == 'state-healthy':
-        status = 'OK'
-        status_info = ''
+    system.add_row(['Switch state', state_info, state])
+
+    for component in r.findall('.//system-monitor:component-status', ns):
+        name = component.find('./system-monitor:component-name', ns).text
+        name = name.replace(' monitor', '')
+        status_raw = component.find('./system-monitor:component-state', ns).text
+        if status_raw == 'state-healthy':
+            status = 'OK'
+            status_info = ''
+            check_status.update(OK)
+        else:
+            status = 'CRIT'
+            status_info = status_raw
+            check_status.update(CRIT)
+        system.add_row([name, status_info, status])
+
+    system = str(system)
+    system = system.replace('\n', '\n\t')
+    system = '\t' + system + '\n'
+    print(system)
+
+    print('Virtual Cluster Switch\n')
+
+    vcs = PrettyTable()
+    vcs.field_names = ['item', 'info', 'status']
+    vcs.header = False
+    vcs.align = 'l'
+
+    r = c.post('http://{}/rest/operational-state/show-vcs'.format(cluster_address),
+               data='<show-vcs></show-vcs>').content
+    r = ElementTree.fromstring(r)
+
+    cluster_generic_status = r.find('.//vcs:cluster-generic-status', ns).text
+    cluster_specific_status = r.find('.//vcs:cluster-specific-status', ns).text
+    total_nodes = r.find('.//vcs:total-nodes-in-cluster', ns).text
+    disconnected_nodes = int(
+        r.find('.//vcs:nodes-disconnected-from-cluster', ns).text)
+
+    cluster_status = 'OK' if cluster_generic_status == 'Good' else 'CRIT'
+    if cluster_status == 'OK':
         check_status.update(OK)
     else:
-        status = 'CRIT'
-        status_info = status_raw
         check_status.update(CRIT)
-    system.add_row([name, status_info, status])
 
-system = str(system)
-system = system.replace('\n', '\n\t')
-system = '\t' + system + '\n'
-print(system)
+    vcs.add_row(['Cluster Status', cluster_specific_status, cluster_status])
+    vcs.add_row(['Total Nodes', total_nodes, ''])
+    vcs.add_row(['Disconnected Nodes',
+                 disconnected_nodes, 'CRIT' if disconnected_nodes else 'OK'])
 
-print('Virtual Cluster Switch\n')
+    vcs = str(vcs)
+    vcs = vcs.replace('\n', '\n\t')
+    vcs = '\t' + vcs + '\n'
+    print(vcs)
 
-vcs = PrettyTable()
-vcs.field_names = ['item', 'info', 'status']
-vcs.header = False
-vcs.align = 'l'
+    rbridges = PrettyTable()
+    rbridges.field_names = ['RBridge', 'Name', 'Serial', 'Status',
+                            'Principal', 'Co-Ordinator', 'Firmware', 'Fabric']
+    rbridges.align = 'l'
 
-r = c.post('http://{}/rest/operational-state/show-vcs'.format(host_fqdn),
-           data='<show-vcs></show-vcs>').content
-r = ElementTree.fromstring(r)
+    for rbridge in r.findall('.//vcs:vcs-node-info', ns):
+        rbridge_id = rbridge.find('./vcs:node-rbridge-id', ns).text
+        name = rbridge.find('./vcs:node-switchname', ns).text
+        serial = rbridge.find('./vcs:node-serial-num', ns).text
+        status = rbridge.find('./vcs:node-condition', ns).text
+        status2 = rbridge.find('./vcs:node-status', ns).text
+        status3 = rbridge.find('./vcs:node-state', ns).text
+        is_principal = rbridge.find('./vcs:node-is-principal', ns).text == 'true'
+        is_coordinator = rbridge.find('./vcs:co-ordinator', ns).text == 'true'
+        firmware = rbridge.find('./vcs:firmware-version', ns).text
+        fabric_state = rbridge.find('./vcs:node-fabric-state', ns).text
 
-cluster_generic_status = r.find('.//vcs:cluster-generic-status', ns).text
-cluster_specific_status = r.find('.//vcs:cluster-specific-status', ns).text
-total_nodes = r.find('.//vcs:total-nodes-in-cluster', ns).text
-disconnected_nodes = int(r.find('.//vcs:nodes-disconnected-from-cluster', ns).text)
+        rbridges.add_row(
+            [rbridge_id, name, serial,
+             '{}, {}, {}'.format(status, status2, status3),
+             '*****' if is_principal else '',
+             '*****' if is_coordinator else '', firmware, fabric_state])
 
-cluster_status = 'OK' if cluster_generic_status == 'Good' else 'CRIT'
-if cluster_status == 'OK':
-    check_status.update(OK)
-else:
-    check_status.update(CRIT)
+    rbridges = str(rbridges)
+    rbridges = rbridges.replace('\n', '\n\t')
+    rbridges = '\t' + rbridges + '\n'
+    print(rbridges)
 
-vcs.add_row(['Cluster Status', cluster_specific_status, cluster_status])
-vcs.add_row(['Total Nodes', total_nodes, ''])
-vcs.add_row(['Disconnected Nodes', disconnected_nodes, 'CRIT' if disconnected_nodes else 'OK'])
+    print('Last 10 log messages\n')
 
-vcs = str(vcs)
-vcs = vcs.replace('\n', '\n\t')
-vcs = '\t' + vcs + '\n'
-print(vcs)
+    log = PrettyTable()
+    log.field_names = ['time', 'severity', 'type', 'message']
+    log.header = False
+    log.align = 'l'
+    log.sortby = 'time'
+    log.reversesort = True
+    log.end = 10
 
+    r = c.post('http://{}/rest/operational-state/show-raslog'.format(cluster_address),
+               data='<show-raslog><rbridge-id>{}</rbridge-id></show-raslog>'.format(rbridge_id)).content
+    r = ElementTree.fromstring(r)
 
-rbridges = PrettyTable()
-rbridges.field_names = ['RBridge', 'Name', 'Serial', 'Status', 'Principal', 'Co-Ordinator', 'Firmware', 'Fabric']
-rbridges.align = 'l'
+    for entry in r.findall('.//raslog:raslog-entries', ns):
+        time = entry.find('./raslog:date-and-time-info', ns).text
+        time = datetime.datetime.strptime(time, '%Y/%m/%d-%H:%M:%S:%f').replace(tzinfo=pytz.UTC)
+        time = time.astimezone(LOCAL_TIME)
+        severity = entry.find('./raslog:severity', ns).text
+        type = entry.find('./raslog:log-type', ns).text
+        message = entry.find('./raslog:message', ns).text
+        message = message.replace('\n.', '')
+        if 'Login information: User [user] Last Successful Login Time' in message:
+            continue
+        log.add_row([time, severity, type, message])
 
-for rbridge in r.findall('.//vcs:vcs-node-info', ns):
-    rbridge_id = rbridge.find('./vcs:node-rbridge-id', ns).text
-    name = rbridge.find('./vcs:node-switchname', ns).text
-    serial = rbridge.find('./vcs:node-serial-num', ns).text
-    status = rbridge.find('./vcs:node-condition', ns).text
-    status2 = rbridge.find('./vcs:node-status', ns).text
-    status3 = rbridge.find('./vcs:node-state', ns).text
-    is_principal = rbridge.find('./vcs:node-is-principal', ns).text == 'true'
-    is_coordinator = rbridge.find('./vcs:co-ordinator', ns).text == 'true'
-    firmware = rbridge.find('./vcs:firmware-version', ns).text
-    fabric_state = rbridge.find('./vcs:node-fabric-state', ns).text
+    log = str(log)
+    log = log.replace('\n', '\n\t')
+    log = '\t' + log + '\n'
+    print(log)
 
-    rbridges.add_row([rbridge_id, name, serial, '{}, {}, {}'.format(status, status2, status3),
-              '*****' if is_principal else '', '*****' if is_coordinator else '', firmware, fabric_state])
+    print('Last 10 important messages\n')
 
-rbridges = str(rbridges)
-rbridges = rbridges.replace('\n', '\n\t')
-rbridges = '\t' + rbridges + '\n'
-print(rbridges)
+    log = PrettyTable()
+    log.field_names = ['time', 'severity', 'type', 'message']
+    log.header = False
+    log.align = 'l'
+    log.sortby = 'time'
+    log.reversesort = True
+    log.end = 10
 
-print('Last 10 log messages\n')
+    for entry in r.findall('.//raslog:raslog-entries', ns):
+        time = entry.find('./raslog:date-and-time-info', ns).text
+        time = datetime.datetime.strptime(time, '%Y/%m/%d-%H:%M:%S:%f').replace(tzinfo=pytz.UTC)
+        time = time.astimezone(LOCAL_TIME).strftime('%Y-%m-%d %H:%M:%S')
+        severity = entry.find('./raslog:severity', ns).text
+        if severity in ['warning', 'informational']:
+            continue
+        type = entry.find('./raslog:log-type', ns).text
+        message = entry.find('./raslog:message', ns).text
+        message = message.replace('\n.', '')
+        log.add_row([time, severity, type, message])
 
-log = PrettyTable()
-log.field_names = ['time', 'severity', 'type', 'message']
-log.header = False
-log.align = 'l'
-log.sortby = 'time'
-log.reversesort = True
-log.end = 10
-
-r = c.post('http://{}/rest/operational-state/show-raslog'.format(host_fqdn),
-           data='<show-raslog></show-raslog>').content
-r = ElementTree.fromstring(r)
-
-for entry in r.findall('.//raslog:raslog-entries', ns):
-    time = entry.find('./raslog:date-and-time-info', ns).text
-    severity = entry.find('./raslog:severity', ns).text
-    type = entry.find('./raslog:log-type', ns).text
-    message = entry.find('./raslog:message', ns).text
-    message = message.replace('\n.', '')
-    if 'Login information: User [user] Last Successful Login Time' in message:
-        continue
-    log.add_row([time, severity, type, message])
-
-log = str(log)
-log = log.replace('\n', '\n\t')
-log = '\t' + log+ '\n'
-print(log)
-
-
-print('Last 10 important messages\n')
-
-log = PrettyTable()
-log.field_names = ['time', 'severity', 'type', 'message']
-log.header = False
-log.align = 'l'
-log.sortby = 'time'
-log.reversesort = True
-log.end = 10
-
-for entry in r.findall('.//raslog:raslog-entries', ns):
-    time = entry.find('./raslog:date-and-time-info', ns).text
-    severity = entry.find('./raslog:severity', ns).text
-    if severity in ['warning', 'informational']:
-        continue
-    type = entry.find('./raslog:log-type', ns).text
-    message = entry.find('./raslog:message', ns).text
-    message = message.replace('\n.', '')
-    log.add_row([time, severity, type, message])
-
-log = str(log)
-log = log.replace('\n', '\n\t')
-log = '\t' + log+ '\n'
-print(log)
-print('All times given in {}'.format(LOCAL_TIME))
-sys.exit(check_status.status)
+    log = str(log)
+    log = log.replace('\n', '\n\t')
+    log = '\t' + log + '\n'
+    print(log)
+    print('All times given in {}'.format(LOCAL_TIME))
+    sys.exit(check_status.status)
